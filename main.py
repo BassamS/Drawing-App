a15e063fd3b3cf3a627f3ebf5ae2c3cafc3f58d1
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-22 15:41:51 +0200

    Quit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import utils
+from utils import *
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+# Name on the top of the window
+pygame.display.set_caption('Drawing App')
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
