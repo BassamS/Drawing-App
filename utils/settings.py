c6563a965567380f822e3d24119637d85776a1fe
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-22 15:29:09 +0200

    Settings

--- utils/settings.py
+++ utils/settings.py
@@ -11,3 +11,17 @@ GREEN = (0, 0, 255)
 FPS = 60
 
 WIDTH, HEIGHT = 600, 700
+
+ROWS = COLS = 50
+
+TOOLBAR_HEIGHT = HEIGHT - WIDTH
+
+PIXEL_SIZE = WIDTH // COLS
+
+BG_COLOR = WHITE
+
+DRAW_GRID_LINES = False
+
+
+def get_font(size):
+    return pygame.font.SysFont('comicsans', size)
