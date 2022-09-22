fe9bd640c3ed911baa508596e40e27deb2230989
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-22 15:51:42 +0200

    clock

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Drawing App')
 
 run = True
+clock = pygame.time.Clock()
 
 while run:
+    clock.tick(FPS)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
