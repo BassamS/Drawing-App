c262e797c264f34dbf8af6effda3a6aed944bb5e
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-23 15:05:09 +0200

    button => draw

--- utils/button.py
+++ utils/button.py
@@ -10,3 +10,19 @@ class Button:
         self.color = color
         self.text = text
         self.text_color = text_color
+
+
+def draw(self, win):
+    pygame.draw.rect(
+        win, self.color, (self.x, self.y, self.width, self.height))
+    pygame.draw.rect(
+        win, BLACK, (self.x, self.y, self.width, self.height), 2)
+    if self.text:
+        button_font = get_font(22)
+        text_surface = button_font.render(self.text, 1, self.text_color)
+        win.blit(text_surface, (self.x + self.width /
+                 2 - text_surface.get_width()/2, self.y + self.height/2 - text_surface.get_height()/2))
+
+
+def clicked(self, po):
+    pass
